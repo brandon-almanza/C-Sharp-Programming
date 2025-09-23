3423882e56bc775283c84325e870f18b2a48689e
Brandon <brandonalmanza2109@gmail.com>
2025-09-23 00:24:48 -0400

    Add files via upload

---
+++ Employee Management with LINQ/A4_BrandonArgenalAlmanza/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4_BrandonArgenalAlmanza
+{
+    public class Employee
+    {
+        public int EmployeeID { get; }
+        public string Name { get; }
+        public string Designation { get; }
+        public bool IsManager { get; }
+        public int hireYear { get; }
+        public string Department { get; }
+
+        public Employee(int id, string name, string designation, bool mgr, int year, string department)
+        {
+            this.EmployeeID = id;
+            this.Name = name;
+            this.Designation = designation;
+            this.IsManager = mgr;
+            this.hireYear = year;
+            this.Department = department;
+        }
+
+        public static List<Employee> EmployeeList = new List<Employee>
+            {
+                new Employee(111, "Pail Willingam", "General Manager", true, 2000, "IT"),
+                new Employee(112, "Alanah Stanning", "Food Chemist", false, 2021, "Sales"),
+                new Employee(123, "Tate Slides", "Help Desk Operator", false, 2019, "Accounts"),
+                new Employee(114, "Edna Skerratt", "Chief Design Engineer", false,2022,  "Manufacturer"),
+                new Employee(245, "Ulrick Costelloe", "Geologist IV", false, 2015, "Manufacturer"),
+                new Employee(616, "Mollie Pfeuffer", "Environmental Tech", false, 2014, "Accounts"),
+                new Employee(193, "Son Croser", "Engineer III", false, 2021, "Manufacturer"),
+                new Employee(3241,"Noemi Appleby", "Health Coach II", true, 2017,"Accounts"),
+                new Employee(2719, "Upton Mander", "Food Chemist", false, 2018, "Human Resources"),
+                new Employee(618, "Delano Shieldon", "Civil Engineer", false, 2018, "Sales"),
+                new Employee(819, "Andres Ioselev", "Environmental Tech", false, 2018, "Human Resources"),
+                new Employee(172, "Art Warkup", "VP Accounting", true, 2007, "Sales"),
+                new Employee(883, "Gusta Elt", "GIS Operator", false, 2001, "IT")
+            };
+
+        public override string ToString()
+        {
+            return $"{this.EmployeeID} --- {this.Name} --- {this.Designation} --- {this.hireYear} --- {(this.IsManager ? "Manager ---" : "")} {this.Department}";
+        }
+    }
+}
---
+++ Employee Management with LINQ/A4_BrandonArgenalAlmanza/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace A4_BrandonArgenalAlmanza
+{
+    internal class Program
+    {
+        static List<Employee> Employees = Employee.EmployeeList;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brandon Argenal Almanza - 301467830");
+            TestLINQ();
+        }
+        static void TestLINQ()
+        {
+            Console.WriteLine("Displaying all employees in INCREASING ORDER by NAMES");
+            var r1 = from employ in Employees
+                     orderby employ.Name ascending
+                     select employ;
+            Console.WriteLine($"{string.Join("\n", r1)}");
+
+
+            Console.WriteLine($"\nDisplaying all employees who are MANAGERS");
+            var r2 = from employ in Employees
+                     where employ.IsManager == true
+                     select employ;
+            Console.WriteLine($"{string.Join("\n", r2)}");
+
+
+            Console.WriteLine("\nDisplaying all employees in HUMAN RESOURCES in INCREASING ORDER of EmployeeID.");
+            var r3 = from employ in Employees
+                     where employ.Department == "Human Resources"
+                     orderby employ.EmployeeID ascending
+                     select employ;
+            Console.WriteLine($"{string.Join("\n", r3)}");
+
+
+            Console.WriteLine("\nDisplaying all employees hired AFTER 2020.");
+            var r4 = Employees.Where(employ => employ.hireYear > 2020);
+            Console.WriteLine($"{string.Join("\n", r4)}");
+
+
+            Console.WriteLine("\nDisplaying the employee who is MANAGER in IT DEPT");
+            var r5 = Employees.Where(employ => employ.Department == "IT" && employ.IsManager == true);
+            Console.WriteLine($"{string.Join("\n", r5)}");
+
+
+            Console.WriteLine($"\nDisplaying all employees who are OPERATORS sorted by DEPT NAME");
+            var r6 = from employ in Employees
+                     where employ.Designation.Contains("Operator")
+                     orderby employ.Department
+                     select employ;
+            Console.WriteLine($"{string.Join("\n", r6)}");
+
+
+            Console.WriteLine($"\nDisplaying all employees grouped by DEPARTMENTS");
+            var r7 = Employees.GroupBy(employ => employ.Department);
+            
+            foreach ( var group in r7)
+            {
+                Console.WriteLine($"\n{group.Count()} employees in {group.Key}");
+                Console.WriteLine($"{string.Join("\n", group)}");
+            }
+                
+        }
+    }
+}
